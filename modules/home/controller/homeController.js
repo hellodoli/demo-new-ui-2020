2b32d7652ec394da6c5328655c2482e66b90f119
PhuPT5 <phupt5@fpt.com.vn>
2020-04-06 15:32:31 +0700

    update keyevent

--- modules/home/controller/homeController.js
+++ modules/home/controller/homeController.js
@@ -1,12 +1,22 @@
 app.controller('homeController', function ($scope, $state) {
   $scope.$on('$stateChangeSuccess', function (event, toState) {
+    document.addEventListener("keydown", myFunctionKey);
     var parents = ['home'];
     $scope.hideParent = parents.indexOf(toState.name) !== -1 ? false : true;
     if (!$scope.hideParent) {
       // TO DO
     }
   });
-
+  
+  function myFunctionKey(e) {
+    switch(e.keyCode){
+      case 13:
+          $state.go('vod', { eventData: { data: 'demo Data Text' } });
+          console.log('key press home');
+          document.removeEventListener("keydown", myFunctionKey);
+          break;
+    }
+  }
   $scope.welcome = 'Welcome Home!!!';
   $scope.goVODState = function () {
     $state.go('vod', { eventData: { data: 'demo Data Text' } });
--- modules/vod/controller/vodController.js
+++ modules/vod/controller/vodController.js
@@ -1,9 +1,21 @@
 app.controller('vodController', function ($scope, $state, $stateParams) {
   $scope.$on('$stateChangeSuccess', function (event, toState) {
     var parents = ['vod'];
+    document.addEventListener("keydown", myFunctionKey);
     $scope.hideParent = parents.indexOf(toState.name) !== -1 ? false : true;
     if (!$scope.hideParent) {
       console.log($stateParams);
     }
   });
+
+  
+  function myFunctionKey(e) {
+    switch(e.keyCode){
+      case 37:
+          $state.go('home');
+          console.log('key press vod');
+          document.removeEventListener("keydown", myFunctionKey);
+          break;
+    }
+  }
 });
